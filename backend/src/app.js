17d7d09107122a01ae17cf7f9270e3c388dfe8b3
Thiago martins <thiagoribeirodossantos965@gmail.com>
2026-03-25 01:49:33 -0300

    Configurando o app.js pra servir arquivos estaticos do frontend por servidor backend

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-
+const path = require("path");
 const recoveryRoutes = require("./routes/recoveryRoutes");
 
 const app = express();
@@ -8,6 +8,22 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+// caminho da raiz (onde estão index.html e assets)
+const publicPath = path.join(__dirname, "../../");
+
+// arquivos físicos (CSS, JS, Imagens)
+app.use(express.static(publicPath));
+
+// 2. Depois, as rotas da sua API
 app.use("/api/recovery", recoveryRoutes);
+
+
+app.use((req, res, next) => {
+  if (req.method === 'GET' && !req.path.startsWith('/api')) {
+    res.sendFile(path.join(publicPath, "index.html"));
+  } else {
+    next();
+  }
+});
 
 module.exports = app;
